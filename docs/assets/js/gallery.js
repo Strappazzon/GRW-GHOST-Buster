18db69439b7aa260d79abed60363e53d8171f925
Strappazzon <strappazzon@protonmail.com>
2020-01-10 23:08:13 +0100

    docs: Add screenshots gallery

---
+++ docs/assets/js/gallery.js
@@ -0,0 +1,10 @@
+---
+---
+
+// http://www.new2html.com/tutorial/using-javascript-change-image-src-attribute/
+function changeScr(scrName) {
+	let scr = document.getElementById('scr');
+	let scra = document.getElementById('scr-a');
+	scr.src = "{{ '/assets/img/' | relative_url }}" + scrName;
+	scra.href = "{{ '/assets/img/' | relative_url }}" + scrName;
+}
