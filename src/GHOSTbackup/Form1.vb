8f3324e1ec999d167ad864c5d22a79deb9378b88
Strappazzon <strappazzon@protonmail.com>
2020-01-11 20:51:25 +0100

    Bump version

--- src/GHOSTbackup/Form1.vb
+++ src/GHOSTbackup/Form1.vb
@@ -5,8 +5,8 @@ Imports System.Text
 Imports Microsoft.Win32
 
 Public Class Form1
-    Public ReadOnly versionCode As Short = 10
-    Public ReadOnly version As String = "1.4.1"
+    Public ReadOnly versionCode As Short = 11
+    Public ReadOnly version As String = "1.4.2"
     Public isGameInstalled As Boolean
     Public isUplayInstalled As Boolean
     Public gamePath As String
