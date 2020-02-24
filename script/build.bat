4e2d67179a870df0999e7aafc6856f295fb06005
Strappazzon <strappazzon@protonmail.com>
2020-02-24 18:58:20 +0100

    Add script to build website and move scripts inside dedicated folder

---
+++ script/build.bat
@@ -0,0 +1,3 @@
+@echo off
+"%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "%~dp0\..\src\GHOSTbackup.sln" /t:Build /p:Configuration=Release
+pause
---
+++ script/jekyll-serve.bat
@@ -0,0 +1,1 @@
+jekyll serve -s "%~dp0..\docs" -d "%~dp0..\docs\_site"
