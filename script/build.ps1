66c5554d95599ba993ffccbcbeb0ef3a8a87cadf
Strappazzon <strappazzon@protonmail.com>
2020-03-27 13:09:42 +0100

    Update scripts and add them to VS Code tasks [travis skip]

--- script/build.ps1
+++ script/build.ps1
@@ -1,1 +1,4 @@
-Start-Process -FilePath ${env:windir}"\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" -ArgumentList "GHOSTbackup.sln /t:Build /p:Configuration=Release" -WorkingDirectory "..\src\" -NoNewWindow
+Remove-Item -LiteralPath "..\src\GHOSTbackup\bin\Release" -Recurse -ErrorAction SilentlyContinue
+Remove-Item -LiteralPath "..\src\GHOSTbackup\obj\Release" -Recurse -ErrorAction SilentlyContinue
+Start-Process -FilePath ${env:windir}"\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" -ArgumentList ${PSScriptRoot}"\..\src\GHOSTbackup.sln /t:Build /p:Configuration=Release" -NoNewWindow
+Pause
