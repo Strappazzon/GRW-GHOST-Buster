d005cb1f022715785b4ef044047b7ce609ddbdf6
Strappazzon <strappazzon@protonmail.com>
2020-04-16 12:07:47 +0200

    Disable Restore context menu item when the game is running

--- src/GHOSTbackup/GHOSTbackup.Helpers/ProcessHelper.vb
+++ src/GHOSTbackup/GHOSTbackup.Helpers/ProcessHelper.vb
@@ -40,9 +40,11 @@ Public Class ProcessHelper
         If WildlandsProc.Count > 0 Then
             IsGameRunning = True
             Form1.PlayGameBtn.Enabled = False
+            Form1.RestoreManageContextMenuItem.Enabled = False
         Else
             IsGameRunning = False
             Form1.PlayGameBtn.Enabled = True
+            Form1.RestoreManageContextMenuItem.Enabled = True
 
             If IsBackupRunning = True Then
                 StopBackup()
