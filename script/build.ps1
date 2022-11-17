77f581ad11f85f3f02e9f2d4b48afbde91cc3359
Alberto Strappazzon <a@strappazzon.xyz>
2022-11-17 17:14:52 +0100

    Update build and release scripts

--- script/build.ps1
+++ script/build.ps1
@@ -1,9 +1,16 @@
-$ResGen = "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\ResGen.exe"
-$ResourcesDir = @(Get-ChildItem ${PSScriptRoot}"\..\src\GHOSTbackup\GHOSTbackup.Localization\*.resx")
+$ResGen = 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8.1 Tools\ResGen.exe'
+$ResourcesDir = @( Get-ChildItem ${PSScriptRoot}"\..\src\GHOSTbackup\GHOSTbackup.Localization\*.resx" )
 
-Remove-Item -LiteralPath "..\src\GHOSTbackup\bin\Release" -Recurse -ErrorAction SilentlyContinue
-Remove-Item -LiteralPath "..\src\GHOSTbackup\obj\Release" -Recurse -ErrorAction SilentlyContinue
-Start-Process -FilePath ${env:windir}"\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" -ArgumentList ${PSScriptRoot}"\..\src\GHOSTbackup.sln /t:Build /p:Configuration=Release" -NoNewWindow
-foreach (${Resource} in ${ResourcesDir}) { Start-Process -FilePath ${ResGen} -ArgumentList ${Resource} -NoNewWindow }
+# Cleanup
+Remove-Item -LiteralPath '..\src\GHOSTbackup\bin\Release' -Recurse -ErrorAction SilentlyContinue
+Remove-Item -LiteralPath '..\src\GHOSTbackup\obj\Release' -Recurse -ErrorAction SilentlyContinue
+
+# Build Solution
+Start-Process -FilePath ${env:ProgramFiles}"\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" -ArgumentList ${PSScriptRoot}"\..\src\GHOSTbackup.sln /t:Build /p:Configuration=Release" -NoNewWindow
+
+# Build localization
+foreach (${Resource} in ${ResourcesDir}) {
+	Start-Process -FilePath ${ResGen} -ArgumentList ${Resource} -NoNewWindow
+}
 
 Pause
--- script/release.ps1
+++ script/release.ps1
@@ -1,33 +1,37 @@
-$7Zip = ${env:ProgramFiles}+"\7-Zip\7z.exe"
-$Resources = @(Get-ChildItem  ".\src\GHOSTbackup\GHOSTbackup.Localization\*.resources")
+# If you want to create a release archive yourself edit 7zip variable first
 
-# Create the release folder if it doesn't exist
-if (-not (Test-Path -Path ".\.release")) {
-	New-Item -Path ".\.release" -ItemType Directory -ErrorAction Stop | Out-Null
+$7zip = ${env:USERPROFILE} + '\scoop\apps\7zip\current\7z.exe'
+$Resources = @( Get-ChildItem '.\src\GHOSTbackup\GHOSTbackup.Localization\*.resources' )
+
+# Create release folder
+if (!(Test-Path -Path '.\.release')) {
+	New-Item -Path '.\.release' -ItemType Directory -ErrorAction Stop | Out-Null
 }
 
-# Copy compiled binaries and other files inside the release folder
-if (Test-Path -LiteralPath ".\src\GHOSTbackup\bin\Release\GHOSTbackup.exe") {
-	Write-Host "Calculating SHA256 checksum."
-	Get-FileHash -LiteralPath ".\src\GHOSTbackup\bin\Release\GHOSTbackup.exe" -Algorithm SHA256 | Out-File -FilePath ".\.release\checksum.sha256" -Encoding utf8 -Width 80
-	Write-Host "Copying compiled binaries and other files."
-	Copy-Item -LiteralPath ".\src\GHOSTbackup\bin\Release\GHOSTbackup.exe" -Destination ".\.release\GHOST Buster.exe" -Force
-	Copy-Item -LiteralPath ".\src\GHOSTbackup\bin\Release\INIFileParser.dll" -Destination ".\.release\INIFileParser.dll" -Force
-	Copy-Item -Path ".\src\Licenses" -Destination ".\.release\" -Recurse -Force
-	Copy-Item -LiteralPath ".\CHANGELOG.txt" -Destination ".\.release\CHANGELOG.txt" -Force
-	Write-Host "Copying localization files."
-	# Create Languages folder if it doesn't exist
-	if (-not (Test-Path -Path ".\.release\Languages")) {
-		New-Item -Path ".\.release\Languages" -ItemType Directory -ErrorAction Stop | Out-Null
+if (Test-Path -LiteralPath '.\src\GHOSTbackup\bin\Release\GHOSTbackup.exe') {
+
+	# Copy build output inside the release folder
+	(Get-FileHash -LiteralPath '.\src\GHOSTbackup\bin\Release\GHOSTbackup.exe' -Algorithm SHA256).Hash | Out-File '.\.release\checksum.sha256' -Encoding utf8
+	Copy-Item -LiteralPath '.\src\GHOSTbackup\bin\Release\GHOSTbackup.exe' -Destination '.\.release\GHOST Buster.exe' -Force
+	Copy-Item -LiteralPath '.\src\GHOSTbackup\bin\Release\INIFileParser.dll' -Destination '.\.release\INIFileParser.dll' -Force
+	Copy-Item -LiteralPath '.\src\GHOSTbackup\bin\Release\Microsoft.Toolkit.Uwp.Notifications.dll' -Destination '.\.release\Microsoft.Toolkit.Uwp.Notifications.dll' -Force
+	Copy-Item -Path '.\src\Licenses' -Destination '.\.release\' -Recurse -Force
+	Copy-Item -LiteralPath '.\CHANGELOG.txt' -Destination '.\.release\CHANGELOG.txt' -Force
+
+	# Copy languages
+	if (!(Test-Path -Path '.\.release\Languages')) {
+		New-Item -Path '.\.release\Languages' -ItemType Directory -ErrorAction Stop | Out-Null
 	}
-	foreach (${Resource} in ${Resources}) { Copy-Item -LiteralPath ${Resource} -Destination ".\.release\Languages\" -Force }
+	foreach (${Resource} in ${Resources}) {
+		Copy-Item -LiteralPath ${Resource} -Destination '.\.release\Languages\' -Force
+	}
 
 	# Create release archive
-	Write-Host "Creating release archive."
-	Remove-Item ".\.release\*.7z" -ErrorAction SilentlyContinue # Remove old release archive to avoid adding it in the new archive
-	Start-Process -FilePath ${7zip} -WorkingDirectory ".\.release" -ArgumentList "a -t7z GHOSTbackup_release.7z .\*" -NoNewWindow
+	Remove-Item '.\.release\*.7z' -ErrorAction SilentlyContinue # Remove old release archive to avoid adding it in the new archive
+	Start-Process -FilePath ${7zip} -WorkingDirectory '.\.release' -ArgumentList 'a -t7z GHOSTbackup_release.7z .\*' -NoNewWindow
+
 } else {
-	Write-Warning "GHOST Buster binaries not found. Compile it first!"
+	Write-Warning 'GHOST Buster binaries not found. Compile it first!'
 }
 
 Pause
